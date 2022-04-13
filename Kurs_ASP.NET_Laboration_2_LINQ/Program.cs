6a7d9062d0a5dcb94603d2e34eeea830278b5046
mabl0200 <100345848+mabl0200@users.noreply.github.com>
2022-04-13 13:11:03 +0200

    Added AddTeacher and AddSchoolClass metods + added objects in DB

--- Kurs_ASP.NET_Laboration_2_LINQ/Program.cs
+++ Kurs_ASP.NET_Laboration_2_LINQ/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kurs_ASP.NET_Laboration_2_LINQ.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Kurs_ASP.NET_Laboration_2_LINQ
 {
@@ -6,7 +8,58 @@ namespace Kurs_ASP.NET_Laboration_2_LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //AddSchoolClass(); /*Metod för att lägga till skolklass*/
+            //AddTeacher(); /*Metod för att lägga till lärare*/
+        }
+        public static void AddSchoolClass() /*Metod för att lägga till skolklass*/
+        {
+            try
+            {
+                using LinqSchoolDB context = new LinqSchoolDB();
+
+                SchoolClass schoolClass = new SchoolClass()
+                {
+                    SchoolClassName = "NV1C"
+                };
+
+                context.Add(schoolClass);
+                context.SaveChanges();
+                Console.WriteLine($"Added {schoolClass.SchoolClassName}");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void AddTeacher() /*Metod för att lägga till lärare*/
+        {
+            try
+            {
+                List<string> FirstNames = new List<string>() { "Johanna", "Simon", "Camilla", "Daniel", "Susanna", "Marcus", "Cecilia", "Richard", "Agnes", "Per", "Petra", "Thomas" };
+                List<string> LastNames = new List<string>() { "Almqvist", "Backlund", "Brolin", "Claesson", "Ekdahl", "Halvarsson", "Lundin", "Schmidt", "Vallin", "Eliasson", "Östlund", "Ali" };
+                int randomFirstName = new Random().Next(0, 12);
+                int randomLastName = new Random().Next(0, 12);
+
+                using LinqSchoolDB context = new LinqSchoolDB();
+
+                Teacher teacher = new Teacher()
+                {
+                    FirstName = FirstNames[randomFirstName],
+                    LastName = LastNames[randomLastName]
+                    
+                };
+
+                context.Add(teacher);
+                context.SaveChanges();
+                Console.WriteLine($"Added {teacher.GetFullName()}");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
     }
 }
